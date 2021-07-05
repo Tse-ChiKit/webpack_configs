6de8b84ff07cd3b9286562d55c2878bce968b78d
Eric Xie <397706572@qq.com>
2021-07-05 22:58:41 +0800

    add devServer config

---
+++ 04devServer/webpack.config.js
@@ -0,0 +1,54 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {resolve} = require('path')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'built.js',
+        path: resolve(__dirname, 'build')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ]
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(jpg|jpeg)$/i,
+                use: [
+                    {
+                      loader: 'url-loader',
+                      options: {
+                        limit: 8 * 1024,
+                        name: '[hash:10].[ext]'
+                      },
+                    },
+                  ],
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ],
+    mode: 'development',
+    devServer: {
+        contentBase: resolve(__dirname, 'build'),
+        compress: true,
+        port: 8888,
+        open: true
+    }
+
+}
