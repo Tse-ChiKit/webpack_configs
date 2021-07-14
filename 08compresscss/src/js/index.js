281e8cdb25f66457939a2b76eb7b6389ca0f10d2
Eric Xie <eric.xie@amway.com>
2021-07-14 15:52:28 +0800

    add compress css config

---
+++ 08compresscss/src/js/index.js
@@ -0,0 +1,4 @@
+import '../css/index.css';
+import '../css/index.scss';
+
+console.log('xie'+2);
---
+++ 08compresscss/webpack.config.js
@@ -0,0 +1,75 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin  = require("optimize-css-assets-webpack-plugin");
+const postcssPresetEnv = require('postcss-preset-env');
+
+const {resolve} = require('path');
+
+process.env.Node_ENV = 'production';
+
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        filename: 'js/built.js',
+        path: resolve(__dirname, 'build')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                              plugins: [
+                                postcssPresetEnv()
+                              ],
+                            }
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(jpg|jpeg)$/i,
+                use: [
+                    {
+                      loader: 'url-loader',
+                      options: {
+                        limit: 8 * 1024,
+                        name: '[hash:10].[ext]',
+                        outputPath: 'imgs'
+                      },
+                    },
+                  ],
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'css/built.css'
+        }),
+        new OptimizeCssAssetsPlugin()
+    ],
+    mode: 'development',
+    devServer: {
+        contentBase: resolve(__dirname, 'build'),
+        compress: true,
+        port: 8888,
+        open: true
+    }
+
+}
